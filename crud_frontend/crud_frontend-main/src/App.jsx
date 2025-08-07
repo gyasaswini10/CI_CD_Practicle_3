52dc2fc242c9b932609a416af9c407a03dad8f03
GURIVI REDDY YASASWINI <gyasu1110@gmail.com>
2025-08-07 11:57:51 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://184.72.122.226:9090/springapp1';
+const BASE_URL = 'http://34.207.239.166:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
